006881298b9574bf1d211b657a6e43784c5fcf90
Robert Mendoza <83177653+Bobisblack19@users.noreply.github.com>
2021-11-16 16:02:17 -0500

    Create api.js

---
+++ api.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const expect = require('chai').expect;
+const ConvertHandler = require('../controllers/convertHandler.js');
+
+module.exports = function (app) {
+  
+  let convertHandler = new ConvertHandler();
+
+
+app.route("/api/convert").get((req, res) => {
+  let input = req.query.input;
+  let initNum = convertHandler.getNum(input);
+  let initUnit = convertHandler.getUnit(input);
+
+  if (!initNum && !initUnit) {
+   return res.send("invalid number and unit")
+  } else if (!initNum) {
+   return res.send("invalid number")
+  } else if (!initUnit) {
+   return res.send('invalid unit')
+  }
+
+  let longInitUnit = convertHandler.spellOutUnit(initUnit)
+  let returnNum = convertHandler.convert(initNum, initUnit);
+  let returnUnit = convertHandler.getReturnUnit(initUnit);
+  let longReturnUnit = convertHandler.spellOutUnit(returnUnit)
+  let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+  res.json({"initNum":initNum,"initUnit":initUnit,"returnNum":parseFloat(returnNum.toFixed(5)),"returnUnit":returnUnit,"string":toString})
+  })
+
+}
+
+})
